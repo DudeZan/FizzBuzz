96f14f9f2242ab19f577abce79ee5cdfe21a8cee
DudeZan <hidayatfauzan291@gmail.com>
2026-09-15 19:42:20 -0400

    Updated Function Multiples, Updated README

--- src/lab1/Multiples.java
+++ src/lab1/Multiples.java
@@ -2,13 +2,19 @@ package lab1;
 
 public class Multiples {
     static void main() {
+        int count = multiples(1000, 3, 5);
+        System.out.println(count);
+    }
+
+    public static int multiples(int n, int a, int b) {
         int count = 0;
         int i = 1;
-        while (i < 1000) {
-            if (i % 3 == 0 || i % 5 == 0){
+        while (i < n) {
+            if (i % a == 0 || i % b == 0){
                 count++;
             }
             i++;
         }
-        System.out.println(count);
-    }}
+        return count;
+    }
+}
