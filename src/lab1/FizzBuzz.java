524e5d81a4e1ef0dd36c063cfbcff0e6e17e39af
DudeZan <hidayatfauzan291@gmail.com>
2026-09-15 19:13:59 -0400

    Updated README, FizzBuzz as a while loop, and Main.java to call FizzBuzz

--- src/lab1/FizzBuzz.java
+++ src/lab1/FizzBuzz.java
@@ -14,7 +14,7 @@ class FizzBuzz {
         }
     }
 
-    private static int getFizzBuzz(int i) {
+    public static int getFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
